8c819651a473755e2a43a55e774d5a6606e7235e
Valerie Pappani <v.pappani@smallstories.fr>
2024-01-11 22:02:07 +0100

    ajout du header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,16 @@
 import { Link } from 'react-router-dom'
+import '../../styles/index.scss'
+import '../../styles/Header.scss'
+import Logo from '../../assets/Kasa-logo.png'
 
 function Header() {
   return (
-    <nav>
-      <Link to="/">Accueil</Link>
-      <Link to="/Propos">À propos</Link>
+    <nav className="navbar">
+      <img src={Logo} alt='logo-kasa' className='navbar__logo' />
+      <div>
+        <Link to="/" className="navbar__link">Accueil</Link>
+        <Link to="/Propos" className="navbar__link">À propos</Link>
+      </div>
     </nav>
   )
 }
--- src/utils/styles/colors.js
+++
@@ -1,8 +0,0 @@
-const colors = {
-    primary: '#FF6060',
-    secondary: '#000000',
-    backgroundLight: '#F6F6F6',
-  }
-  
-  export default colors
-  
