77076a4111e95c05f828e088a12d44b4518cec75
Valerie Pappani <v.pappani@smallstories.fr>
2024-01-12 13:56:06 +0100

    ajout footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,18 @@
+// import { Link } from 'react-router-dom'
+import '../../styles/index.scss'
+import '../../styles/Footer.scss'
+import LogoBlanc from '../../assets/Kasa-logo-blanc.png'
+
+
+function Footer() {
+    return (
+        <footer className='footer'>
+            <div className='footer__elem'>
+                <img src={LogoBlanc} alt='logo kasa blanc' className='footer__elem--logo' />
+                <p className='footer__elem--content'>© 2020 Kasa. All rights reserved</p>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer
--- src/index.jsx
+++ src/index.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Header from './components/Header'
+import Footer from './components/Footer'
 import Home from './pages/Home/'
 import Propos from './pages/Propos'
 import Location from './pages/Location'
@@ -18,6 +19,7 @@ ReactDOM.render(
         <Route path="*" element={<Error />} />
       </Routes>
     </Router>
+    <Footer />
   </React.StrictMode>,
   document.getElementById('root')
 )
