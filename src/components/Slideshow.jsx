81f41a0a54307e6639f4fb7467d066e57a4c7861
Valerie Pappani <v.pappani@smallstories.fr>
2024-04-22 14:54:32 +0200

    ajout carousel

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -1,18 +1,36 @@
-function Slideshow({ ficheSelection }) {    
+import React, { useState } from 'react';
+import '../styles/Slideshow.scss';
+import Left from '../assets/left.png'
+import Right from '../assets/right.png'
+
+function Slideshow({ ficheSelection }) {
+    const [activeIndex, setActiveIndex] = useState(0);
+
+    const nextSlide = () => {
+        setActiveIndex((prevIndex) => (prevIndex === ficheSelection.pictures.length - 1 ? 0 : prevIndex + 1));
+    };
+
+    const prevSlide = () => {
+        setActiveIndex((prevIndex) => (prevIndex === 0 ? ficheSelection.pictures.length - 1 : prevIndex - 1));
+    };
+
+
     return (
-      <div>
-          <div key={ficheSelection.id}>
-            <div>
-              {ficheSelection.pictures.map((picture, index) => {
-                console.log(picture);
-                return (
-                  <img key={index} src={picture} alt={`pic ${index + 1}`} />
-                );
-              })}
+        <div className="slider">
+            <div className="slider__prev" onClick={prevSlide}>
+                <img src={Left} alt="left arrow" />
             </div>
-          </div>
-      </div>
+                {ficheSelection.pictures.map((picture, index) => (
+                    <div key={index} className={index === activeIndex ? 'slide active' : 'slide'}>
+                        <img src={picture} alt={`pic ${index + 1}`} className="slider__img"/>
+                    </div>
+
+                ))}
+            <div className="slider__next" onClick={nextSlide}>
+                <img src={Right} alt="right arrow" />
+            </div>
+        </div>
     );
-  }
+}
 
 export default Slideshow
